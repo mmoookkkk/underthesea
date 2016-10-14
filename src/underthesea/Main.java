31afde320a48a8af162218cd22517d29a1ca07c0
mink <supidsara_tha@hotmail.com>
2016-10-14 16:48:07 +0700

    mink

--- src/underthesea/Main.java
+++ src/underthesea/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello it's kkkkkk");
+		System.out.print("hello it's mink");
 	}
 
 }
