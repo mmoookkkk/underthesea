e9ef4353c0a2b4441e7ca679615cfd9483090392
mmoookkkk <phanthilamook@gmail.com>
2016-10-14 15:51:52 +0700

    second push

--- src/underthesea/Main.java
+++ src/underthesea/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello it's me");
+		System.out.print("hello it's mook");
 	}
 
 }
